55c261767f6d3924da301183a61426c0f41c21fe
Rythovius <evdooren@rythovius.nl>
2025-07-06 20:35:06 +0200

    Fix Netlify API key deployment issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  experimental: {
+    serverComponentsExternalPackages: ['@google/generative-ai']
+  },
+  env: {
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  }
 }
 
 module.exports = nextConfig 
